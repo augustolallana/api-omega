5d2b9f02d2618707716d6157f0b54aa068df4ea4
augustolallana <lallanaaugusto@gmail.com>
2025-06-01 22:10:21 -0300

    Add initial db config

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from src.database.config import create_db_and_tables
 from src.routers import admin, auth, cart, checkout, orders, products, users
+from src.settings import settings
 
-app = FastAPI()
+
+# Runs on every server startup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan events for the FastAPI application."""
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    lifespan=lifespan,
+)
 
 app.include_router(admin.router)
 app.include_router(auth.router)
